ad452baadb4e73378ad48f66515c33363e249546
GaganSingh2332 <gagans0422@gmail.com>
2024-08-27 09:34:47 -0700

    Update Program8.java
    did basic code, need to do max and min code then done.

--- Program.java
+++ Program.java
@@ -1,7 +1,30 @@
 //import stuff here
-
+import java.util.Scanner;
+import java.lang.Math;
 //Your code here
+public class Program8 {
+public static void main(String[] args) {
+int firstNum = 0;
+int secondNum = 0;
+  
+Scanner numScanner = new Scanner(System.in);
+System.out.println("Enter any Integer: ")
+firstNum = numScanner.nextInt();
+System.out.println("Enter another Integer: ")
+secondNum = numScanner.nextInt();
+}
+int sum = firstNum + secondNum;
+int difference = firstNum - secondNum;
+int product = firstNum * secondNum;
+double average = (sum) / 2;
+int distance = Math.abs(difference);
+int max = 
+int min = 
 
+
+
+
+}
 //Paste console output below:
 /*
 
